6b9c07d862e3c4e1b2b903ec97d8684cc32678c6
Devin Lewtan <devinlewtan@gmail.com>
2020-03-31 21:42:50 -0400

    Added schemas for data models

--- db.js
+++ db.js
@@ -1,6 +1,24 @@
 // db.js
 const mongoose = require('mongoose')
 
-// my schema goes here!
+const UserSchema = new mongoose.Schema({
+	email: String,
+  password: String,
+  profile: ProfileSchema,
+});
+
+const ProfileSchema = new mongoose.Schema({
+	user: UserSchema,
+  image: String,
+  questions: [Question],
+});
+
+const QuestionSchema = new mongoose.Schema({
+	label: String,
+  options: [Object]
+});
 
 mongoose.connect(dbconf, {useNewUrlParser: true, useUnifiedTopology: true, } )
+mongoose.model('User', UserSchema);
+mongoose.model('Profile', ProfileSchema);
+mongoose.model('Question', QuestionSchema);
